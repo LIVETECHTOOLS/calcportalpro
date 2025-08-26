0f44befdc21dc1c903310291dbb357733b5adf12
LIVETECHTOOLS INC <info@livetechtools.com>
2025-08-26 20:55:58 +0000

    Add Google Analytics tracking (G-3RWDG6W0T4) to all pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -72,6 +72,19 @@ export default function RootLayout({
         <link rel="icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <link rel="manifest" href="/manifest.json" />
+        
+        {/* Google Analytics */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-3RWDG6W0T4"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-3RWDG6W0T4');
+            `,
+          }}
+        />
       </head>
       <body className={inter.className}>
         {children}
